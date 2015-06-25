ab9227fe92a9c574430a166b0b44eae2ccd2d128
James Sherry <james.sherry@novafabrik.com>
2015-06-25 20:02:14 +0100

    setup packages and initial config

--- client/app.js
+++ client/app.js
@@ -1,22 +1,39 @@
 'use strict';
 
 angular.module('ka2', [
-  'ngRoute',
+  //Native services
   'ngCookies',
   'ngResource',
   'ngSanitize',
   'ngAnimate',
-  'btford.socket-io'
+  'ngTouch',
+
+  //3rd party
+  'btford.socket-io',
+  'ui.router',
+  'ui.grid',
+  'mwl.calendar',
+  'ui.bootstrap',
+  'restangular',
+  'ui.gravatar',
+  'toastr'//,
+  'formly', 
+  'formlyBootstrap'
+  'angularjs-dropdown-multiselect'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
 
-    $routeProvider
-      .otherwise({
-        redirectTo: '/'
-      });
-
+    $urlRouterProvider.otherwise('/');
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
+
+    calendarConfigProvider.configureDateFormats({
+      hour: 'HH:mm' //this will configure the hour view to display in 24 hour format rather than the default of 12 hour
+    });
+
+    calendarConfigProvider.configureTitleFormats({
+      day: 'ddd D MMM' //this will configure the day view title to be shorter
+    });
 
   })
   .factory('authInterceptor',
--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,10 @@ module.exports = function (app) {
 
   // API
   app.use('/api/users', require('./api/user'));
+  app.use('/api/ingredients', require('./api/ingredient'));
+  app.use('/api/recipes', require('./api/recipe'));
+  //app.use('/api/shopping', require('./api/shopping'));
+  //app.use('/api/cupboard', require('./api/cupboard'));
 
   // Auth
   app.use('/auth', require('./auth'));
