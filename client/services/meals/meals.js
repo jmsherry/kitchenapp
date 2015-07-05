8e96c6421bf96f285d622fb0ddb9da62496e0832
James Sherry <james.sherry@novafabrik.com>
2015-07-05 23:46:54 +0100

    meals service

---
+++ client/services/meals/meals.js
@@ -0,0 +1,116 @@
+(function(){
+'use strict';
+
+angular.module('kitchenapp')
+    .factory('Meals', Meals);
+
+    Meals.$inject = ['$rootScope', '$cookieStore', '$q', '$http', '$resource', 'toastr', 'Recipes', 'Cupboard', 'Shopping'];
+
+    function Meals($rootScope, $cookieStore, $q, $http, $resource, toastr, Recipes, Cupboard, Shopping) {
+
+        var _meals = [],
+        _resource = $resource('/api/meals');
+
+         function init() {
+          console.log('firing init');
+          var $deferred = $q.defer();
+          _meals = $deferred.promise;
+
+            function successCB(data){
+              console.log('in successCB', arguments);
+              $deferred.resolve(data);
+              console.log('meals service loaded.', _meals, data);
+            }
+
+            function errCB(err){
+              console.log('in errCB', arguments);
+              $deferred.reject(err);
+              toastr.error('Failed to load meals!', 'Server Error ' + err.status + ' ' + err.data.message);
+            }
+
+            _meals.then(function(data){
+              console.log('in meals then', arguments);
+              console.log('_meals before', _meals);
+              _meals = data;
+              console.log('_meals after', _meals);
+            });
+
+            _resource.query(successCB, errCB);
+         }
+
+        function get() {
+          console.log('GET', _meals);
+          return _meals;
+        }
+
+        //persists to server
+        function save(newMeal) {
+            console.log('save args', arguments);
+            console.log('SAVE _meals: ', _meals);
+            var self = this;
+
+            function successCB(response) {
+              console.log('save successCB: ', response);
+                self.add(response.meal);
+            }
+
+            function errCB(err) {
+                console.log('save errCB: ', err);
+                toastr.error('Failed to add ' + err.config.data.name + "!", 'Server Error ' + err.status + ' ' + err.data.message);
+            }
+
+
+            _resource.save(newMeal, _.bind(successCB, self), _.bind(errCB, self));
+
+        }
+
+        //adds a meal to the users meals array
+        function add(newMeal){
+          console.log('in add:', arguments);
+          var meals = this.get();
+
+          $q.when(meals, function(meals){
+            meals.push(newMeal);
+            toastr.success(newMeal.name + " added!");
+          });
+        }
+
+        // Turns a recipe id into a meal object
+        function createMealObject(id){
+          var recipe = Recipes.getRecipe(id);
+          var mealObj = _.clone(recipe);
+
+          mealObj.isComplete = false;
+          mealObj.ingredients = Cupboard.process(mealObj.ingredients);
+          mealObj.sheduledDate = null;
+          mealObj.recipe = id;
+
+          if(mealObj.ingredients.missing.length === 0){
+            mealObj.isComplete = true;
+          }
+
+          return mealObj;
+        }
+
+        function create(id){
+          var self = this, meal, ingredients;
+          meal = self.createMealObject(id),
+          ingredients = meal.ingredients;
+          Cupboard.bulkRemove(ingredients.present);
+          Shopping.add(ingredients.missing);
+          self.save(meal);
+        }
+
+        init();
+
+        return {
+          init: init,
+          get: get,
+          add: add,
+          save: save,
+          create: create,
+          createMealObject: createMealObject
+        }
+
+    }
+}());
---
+++ client/services/meals/meals.spec.js
@@ -0,0 +1,162 @@
+'use strict';
+
+describe('Service: Meals', function() {
+
+    beforeEach(module('ngMock'));
+    beforeEach(module('ui.router'));
+    beforeEach(module('kitchenapp', 'templates'));
+
+
+    var $httpBackend,
+        $templateCache,
+        $compile,
+        $urlRouterProvider,
+        $rootScope,
+        $provide,
+        Auth,
+        Meals,
+        element,
+        $scope;
+
+
+    beforeEach(inject(function(_$templateCache_, _$httpBackend_, _$rootScope_, _Auth_) {
+        $templateCache = _$templateCache_;
+        $httpBackend = _$httpBackend_;
+        $rootScope = _$rootScope_;
+        Auth = _Auth_;
+    }));
+
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    beforeEach(inject(function(_Meals_) {
+        Meals = _Meals_;
+    }));
+
+    beforeEach(inject(function() {
+      spyOn(Auth, "getUser").and.returnValue({
+        meals: {pending: [{_id: '1', isComplete: false}, {_id: '2', isComplete: false}, {_id: '3', isComplete: false}], 
+        complete: [{_id: 5, isComplete: true}]}
+      });  
+    }));
+
+    it('is a registered service', function() {
+        expect(Meals).not.toBeUndefined();
+    });
+
+
+    describe('\'get\' method', function() {
+
+        it('is defined', function() {
+            expect(Meals.get).not.toBeUndefined();
+        });
+
+        it('it calls to the Auth service to get the user object', function() {
+            var meals = Meals.get();
+            expect(Auth.getUser).toHaveBeenCalled();
+            expect(meals).not.toBeUndefined();
+            expect(_.isObject(meals)).toBe(true);
+            expect(_.isArray(meals.complete)).toBe(true);
+            expect(_.isArray(meals.pending)).toBe(true);
+            expect(meals.pending.length).toEqual(3);
+        });
+
+    });
+
+    describe('\'add\' method', function(){
+
+      beforeEach(inject(function() {
+        spyOn(Meals, "add").and.callThrough();
+      }));
+
+      it('is defined', function() {
+        expect(Meals.add).not.toBeUndefined();
+      });
+
+      it('takes a parameter object and adds it to the array of meals', function(){
+        var testObj = {_id: 4}, meals = Meals.get();
+        Meals.add(testObj);
+        expect(Meals.add).toHaveBeenCalledWith(testObj);
+        //console.log(meals);
+        expect(meals.pending.length).toEqual(4);
+        expect(meals.pending[3]._id).toEqual(4);
+      });
+
+    });
+
+    describe('\'remove\' method', function(){
+
+      beforeEach(inject(function() {
+        spyOn(Meals, "remove").and.callThrough();
+      }));
+
+      it('is defined', function() {
+        expect(Meals.remove).not.toBeUndefined();
+      });
+
+      it('takes a parameter object and adds it to the array of meals', function(){
+        var testObj = {_id: '3', isComplete: false}, 
+        testObjComp = {_id: '5', isComplete: true}, 
+        meals = Meals.get();
+
+        //For pending
+        //console.log(meals.pending);
+        expect(meals.pending.length).toEqual(3);
+        Meals.remove(testObj);
+        expect(Meals.remove).toHaveBeenCalledWith(testObj);
+        expect(meals.pending.length).toEqual(2);
+        expect(_.findWhere(meals.pending, testObj)).toBeUndefined();
+
+        //For complete
+        //console.log(meals.complete);
+        expect(meals.complete.length).toEqual(1);
+        Meals.remove(testObjComp);
+        expect(Meals.remove).toHaveBeenCalledWith(testObjComp);
+        expect(meals.complete.length).toEqual(0);
+        expect(_.findWhere(meals.complete, testObj)).toBeUndefined();
+      });
+
+    });
+
+
+    describe('\'updateUser\' method', function(){
+
+      it('is defined', function() {
+        expect(Meals.updateUser).not.toBeUndefined();
+      });
+
+      it('updates the user object with new values', function(){
+
+        spyOn(Auth, 'updateUser');
+
+        var user = Auth.getUser(),
+        testObj = {_id: '4', isComplete: false}, 
+        testObjComp = {_id: '5', isComplete: true}, 
+        meals = Meals.get();
+
+        expect(user).not.toBeUndefined();
+        expect(_.isObject(user.meals)).toBe(true);
+        expect(_.isArray(user.meals.complete)).toBe(true);
+        expect(_.isArray(user.meals.pending)).toBe(true);
+        expect(user.meals.complete.length).toEqual(1);
+        expect(user.meals.pending.length).toEqual(3);
+
+        Meals.add(testObj);
+        Meals.remove(testObjComp);
+        Meals.updateUser();
+
+        user = Auth.getUser();
+
+        expect(Auth.updateUser).toHaveBeenCalled();
+
+        expect(user.meals.complete.length).toEqual(0);
+        expect(user.meals.pending.length).toEqual(4);
+      });
+
+    });
+
+
+});
