2b508357fcff865522a955a6a73b5188875f9eca
James Sherry <james.sherry@novafabrik.com>
2015-07-05 23:47:11 +0100

    ingredients service

---
+++ client/services/ingredients/ingredients.js
@@ -0,0 +1,88 @@
+(function(){
+'use strict';
+
+angular.module('kitchenapp')
+    .factory('Ingredients', Ingredients);
+
+    Ingredients.$inject = ['$rootScope', '$cookieStore', '$q', '$http', '$resource', 'toastr'];
+
+    function Ingredients($rootScope, $cookieStore, $q, $http, $resource, toastr) {
+
+        var _ingredients = [],
+        _resource = $resource('/api/ingredients');
+
+         function init() {
+          console.log('firing init');
+          var $deferred = $q.defer();
+          _ingredients = $deferred.promise;
+
+            function successCB(data){
+              console.log('in successCB', arguments);
+              $deferred.resolve(data);
+              console.log('Ingredients service loaded.', _ingredients, data);
+            }
+
+            function errCB(err){
+              console.log('in errCB', arguments);
+              $deferred.reject(err);
+              toastr.error('Failed to load ingredients!', 'Server Error ' + err.status + ' ' + err.data.message);
+            }
+
+            _ingredients.then(function(data){
+              console.log('in ings then', arguments);
+              console.log('_ingredients before', _ingredients);
+              _ingredients = data;
+              console.log('_ingredients after', _ingredients);
+            });
+
+            _resource.query(successCB, errCB);
+         }
+
+        function get() {
+          console.log('GET', _ingredients);
+          return _ingredients;
+        }
+
+        function save(newIngredient) {
+            console.log('save args', arguments);
+            console.log('SAVE _ingredients: ', _ingredients);
+            var self = this;
+
+            function successCB(response) {
+              console.log('save successCB: ', response);
+                self.add(response.ingredient);
+            }
+
+            function errCB(err) {
+                console.log('save errCB: ', err);
+                toastr.error('Failed to add ' + err.config.data.name + "!", 'Server Error ' + err.status + ' ' + err.data.message);
+            }
+
+
+            _resource.save(newIngredient, _.bind(successCB, self), _.bind(errCB, self));
+
+        }
+
+        function add(newIng){
+          console.log('in add:', arguments);
+          var self = this,
+          ings = self.get();
+
+          $q.when(ings, function(ings){
+            ings.push(newIng);
+            toastr.success(newIng.name + " added!");
+          });
+        }
+
+        init();
+
+        return {
+          _ings: _ingredients,
+          init: init,
+          get: get,
+          add: add,
+          save: save
+        }
+
+    }
+}());
---
+++ client/services/ingredients/ingredients.spec.js
@@ -0,0 +1,138 @@
+'use strict';
+
+describe('Service: Ingredients', function() {
+
+    beforeEach(module('ngMock'));
+    beforeEach(module('ui.router'));
+    beforeEach(module('kitchenapp', 'templates'));
+    beforeEach(module('toastr'));
+
+
+    var $httpBackend,
+        $q,
+        $templateCache,
+        $compile,
+        $urlRouterProvider,
+        $rootScope,
+        $scope,
+        ingsRequestHandler,
+        Auth,
+        Ingredients,
+        response,
+        toastr;
+
+
+    beforeEach(inject(function(_$templateCache_, _$httpBackend_, _$rootScope_, _Auth_, _$q_) {
+        $templateCache = _$templateCache_;
+        $httpBackend = _$httpBackend_;
+        $rootScope = _$rootScope_;
+        Auth = _Auth_;
+        $q = _$q_
+    }));
+
+
+    beforeEach(inject(function(_Ingredients_) {
+        Ingredients = _Ingredients_;
+    }));
+
+    beforeEach(inject(function(_toastr_) {
+        toastr = _toastr_;
+    }));
+
+    beforeEach(function() {
+    	ingsRequestHandler = $httpBackend.when('GET', '/api/ingredients').respond([{'name': 'init'}]);
+    });
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('is a registered service', function() {
+        $httpBackend.flush();
+        expect(Ingredients).not.toBeUndefined();
+    });
+
+
+
+    it('has an \'init\' method: ', function() {
+        $httpBackend.flush();
+        expect(Ingredients.init).not.toBeUndefined();
+    });
+
+
+
+    describe('The \'init\' method', function(){
+
+        it('is called on load', function(){
+            var ingredients = Ingredients.get();
+            $httpBackend.flush();
+            $q.when(ingredients, function(data){
+                expect(data[0].name).toBe('init');
+            });
+        });
+
+
+    	it('shows a toastr error if the server responds with an error', function(){
+    		ingsRequestHandler.respond(500, {message: 'balls'});
+    		spyOn(toastr, 'error').and.callThrough();
+            $httpBackend.flush();
+    		expect(toastr.error).toHaveBeenCalled();
+    	});
+    });
+
+
+
+    it('has an \'get\' method', function() {
+        $httpBackend.flush();
+        expect(Ingredients.get).not.toBeUndefined();
+    });
+
+    describe('The \'get\' method: ', function(){
+    	it('returns the \'_ingredients\' array', function(){
+    		var ingredients = Ingredients.get();
+            console.log(ingredients);
+            $httpBackend.flush();
+            $q.when(ingredients, function(data){
+        		expect(_.isArray(data)).toBe(true);
+        		expect(data.length).toBe(1);
+            });
+    	});
+    });
+
+    it('has a \'save\' method', function(){
+      $httpBackend.flush();
+      expect(Ingredients.save).not.toBeUndefined();
+    });
+
+    describe('The \'save\' method: ', function(){
+      it('saves to the server then hands off to the add method onsuccess', function(){
+        var testItem = {ingredient: {'name': 'test'}};
+        $httpBackend.expectPOST('/api/ingredients', testItem).respond(null);
+        spyOn(Ingredients, 'add').and.callThrough();
+        Ingredients.save(testItem);
+        $httpBackend.flush();
+        expect(Ingredients.add).toHaveBeenCalled();
+      });
+    });
+
+
+    it('has an \'add\' method: ', function() {
+        $httpBackend.flush();
+        expect(Ingredients.add).not.toBeUndefined();
+    });
+
+    describe('The \'add\' method', function(){
+    	it('adds an item to the \'_ingredients\' array', function(){
+            var testItem = {ingredient: {'name': 'test'}}, ingredients = Ingredients.get();
+
+            Ingredients.add(testItem);
+            $q.when(ingredients, function(data){
+                console.log(data);
+                expect(data.length).toBe(2);
+            });
+            $httpBackend.flush();
+        });
+    });
+
+});
