54eeb86c78416dc6ab38b1e7a7d09b9846c667a4
James Sherry <james.sherry@novafabrik.com>
2015-07-05 23:47:26 +0100

    cupboard service

---
+++ client/services/cupboard/cupboard.js
@@ -0,0 +1,107 @@
+(function() {
+    'use strict';
+
+    angular.module('kitchenapp')
+        .service('Cupboard', Cupboard);
+
+    Cupboard.$inject = ['$q', '$resource', 'Auth', 'Ingredients', 'toastr'];
+
+    function Cupboard($q, $resource, Auth, Ingredients, toastr) {
+
+        var _cupboard = [],
+        _resource = $resource('/api/cupboard');
+
+        this.getCupboard = function getCupboard () {
+            return _cupboard;
+        };
+
+        this.setCupboard = function setCupboard (cupboard) {
+            _cupboard = cupboard;
+        };
+
+        this.getResource = function getResource () {
+            return _resource;
+        };
+
+        this.getOwner = function getOwner (){
+          return Auth.getUser();
+        };
+
+        this.toastr = toastr;
+
+    }
+
+
+    Cupboard.prototype.init = function init() {
+      console.log('cupboard init')
+        var user = this.getOwner();
+        this.setCupboard(user.cupboard);
+    };
+
+    Cupboard.prototype.get = function get() {
+        return this.getCupboard();
+    };
+
+
+    /**
+     * add
+     *
+     * @param ingredient {}
+     * Adds an ingredient to the ingredients array
+     *
+     */
+    Cupboard.prototype.add = function add (ing) {
+
+        function CBSuccess () {
+            this.setCupboard(this.getCupboard().push(ing));
+            this.toastr.success(ing.name + ' has been added');
+        }
+
+        this.getResource().save(ing, _.bind(CBSuccess, this, ing));
+
+    };
+
+    Cupboard.prototype.remove = function remove (ing) {
+        function CBSuccess () {
+            var cupboard = this.getCupboard();
+
+            cupboard.splice(cupboard.indexOf(ing), 1);
+            this.toastr.success(ing.name + ' has been removed');
+        }
+
+        this.getResource().remove(ing, _.bind(CBSuccess, this, ing));
+    };
+
+    Cupboard.prototype.bulkRemove = function remove (ings) {
+      //remove all present ingredients from the users cupboard
+      _.forEach(ings, function(ing){
+        this.remove(ing);
+      });
+
+      return ings;
+    };
+
+
+    Cupboard.prototype.process = function process (idsArray) {
+        var self = this,
+            presentIngredients = [],
+            missingIngredients = [],
+            cupboard = this.getCupboard();
+
+        _.forEach(idsArray, function(thisIngID) {
+            if (cupboard.indexOf(thisIngID) === -1) {
+                missingIngredients.push(thisIngID);
+            } else {
+                presentIngredients.push(thisIngID);
+            }
+        });
+
+
+        return {
+          present: presentIngredients,
+          missing: missingIngredients
+        };
+
+    };
+
+}());
---
+++ client/services/cupboard/cupboard.spec.js
@@ -0,0 +1,162 @@
+'use strict';
+
+describe('Service: Cupboard', function() {
+
+    beforeEach(module('ngMock'));
+    beforeEach(module('ui.router'));
+    beforeEach(module('kitchenapp', 'templates'));
+
+
+    var $httpBackend,
+        $templateCache,
+        $compile,
+        $urlRouterProvider,
+        $rootScope,
+        $provide,
+        Auth,
+        userMock,
+        //initSpy,
+        //authRequestHandler,
+        Cupboard,
+        element,
+        $scope;
+
+
+    beforeEach(inject(function(_$templateCache_, _$httpBackend_, _$rootScope_, _Auth_, _Cupboard_) {
+        $templateCache = _$templateCache_;
+        $httpBackend = _$httpBackend_;
+        $rootScope = _$rootScope_;
+        Auth = _Auth_;
+        Cupboard = _Cupboard_;
+
+        userMock = {
+          cupboard: [{_id: '1'}, {_id: '2'}, {_id: '3'}]
+        };
+
+        spyOn(Auth, "getUser").and.returnValue(userMock);
+
+        // authRequestHandler = $httpBackend.when('GET', '/api/user')
+        //                     .respond({userId: 'userX'}, {'A-Token': 'xxx'});
+    }));
+
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+
+
+    it('is a registered service', function() {
+        expect(Cupboard).not.toBeUndefined();
+    });
+
+
+    it('is has an \'init\' method', function() {
+        expect(Cupboard.init).not.toBeUndefined();
+    });
+
+    describe('\'init\' method', function() {
+      
+      it('calls the Auth service', function() {
+        expect(Auth.getUser).toHaveBeenCalled();
+      });
+
+      it('populates the _cupboard array with the user\'s cupboard', function() {
+        expect(Cupboard.getCupboard()).toBe(userMock.cupboard);
+      });
+
+    });
+
+
+    it('is has a \'get\' method', function() {
+        expect(Cupboard.get).not.toBeUndefined();
+    });
+
+    describe('\'get\' method', function() {
+      it('returns the user\'s cupboard', function(){
+        expect(Cupboard.get()).toBe(userMock.cupboard);
+      });   
+    });
+
+    it('is has a \'add\' method', function() {
+        expect(Cupboard.add).not.toBeUndefined();
+    });
+
+    describe('\'add\' method', function(){
+
+      beforeEach(inject(function() {
+        spyOn(Cupboard, "add").and.callThrough();
+      }));
+
+
+      it('takes a parameter object and adds it to the cupboard array', function(){
+        var testObj = {_id: '4'}, cupboard = Cupboard.get();
+        $httpBackend.expectPOST('/api/cupboard').respond(null);
+
+        Cupboard.add(testObj);
+        $httpBackend.flush();
+
+        expect(Cupboard.add).toHaveBeenCalledWith(testObj);
+        expect(cupboard.length).toBe(4);
+        expect(cupboard[3]._id).toBe('4');
+      });
+
+    });
+
+    it('is has a \'remove\' method', function() {
+        expect(Cupboard.remove).not.toBeUndefined();
+    });
+
+    describe('\'remove\' method', function(){
+
+      beforeEach(inject(function() {
+        spyOn(Cupboard, "remove").and.callThrough();
+        $httpBackend.expectDELETE('/api/cupboard?_id=3').respond();
+      }));
+
+
+      it('takes a parameter object and removes it from the relevant array in cupboard', function(){
+        var testObj = {_id: '3'}, cupboard = Cupboard.get();
+        Cupboard.remove(testObj)
+        expect(Cupboard.remove).toHaveBeenCalledWith(testObj);
+        $httpBackend.flush();
+        expect(cupboard.length).toBe(2);
+        expect(cupboard.indexOf(testObj)).toBe(-1);
+      });
+
+    });
+
+    // it('is has a \'checkIngredients\' method', function() {
+    //     expect(Cupboard.checkIngredients).not.toBeUndefined();
+    // });
+
+    // describe('\'checkIngredients\' method', function(){
+    //     it('which checks whether a series of ingredients are in the cupboard and returns a boolean', function(){
+
+    //     });
+    // });
+
+    it('is has a \'process\' method', function() {
+        expect(Cupboard.process).not.toBeUndefined();
+    });
+
+    describe('\'process\' method', function(){
+      var idsArray = ['1', '2', '4'];
+        beforeEach(function(){
+
+        });
+
+        it('which removes what you have', function(){
+          Cupboard.process(idsArray);
+
+          
+        });
+
+        // it('calls the ShoppingList service to add the missing ingredients', function(){
+
+        // });
+    });   
+
+
+});
