65656f88f4ca51e37f79439dcedc01b1ec05947b
James Sherry <james.sherry@novafabrik.com>
2015-07-05 23:48:08 +0100

    ingredients form directive

---
+++ client/directives/ingredient-form/ingredient-form.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('kitchenapp')
+  .directive('editingredientform', function () {
+    return {
+      restrict: 'E',
+      replace:true,
+      templateUrl: 'directives/ingredient-form/ingredient-form.html',
+    };
+  });
