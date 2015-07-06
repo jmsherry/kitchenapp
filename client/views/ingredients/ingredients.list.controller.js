9181cc5c05e6f27349ab26ee559ae9b76d2952a3
James Sherry <james.sherry@novafabrik.com>
2015-07-06 05:58:32 +0100

    added multi-select capability

--- client/views/ingredients/ingredients.list.controller.js
+++ client/views/ingredients/ingredients.list.controller.js
@@ -4,9 +4,9 @@
 angular.module('kitchenapp')
   .controller('IngredientsCtrl', IngredientsCtrl);
 
-IngredientsCtrl.$inject = ['Ingredients', '$scope', '$q'];
+IngredientsCtrl.$inject = ['Ingredients', '$scope', '$q', '$log', 'uiGridConstants', 'Shopping', 'Cupboard'];
 
-function IngredientsCtrl(Ingredients, $scope, $q) {
+function IngredientsCtrl(Ingredients, $scope, $q, $log, uiGridConstants, Shopping, Cupboard) {
 
 	var vm = this, ingredients, rowTemplate, data = [];
 
@@ -18,7 +18,10 @@ function IngredientsCtrl(Ingredients, $scope, $q) {
          '</div>';
 
 		$scope.gridOptions = {
+      enableRowSelection: true,
+      enableSelectAll: true,
 			enableFiltering: true,
+      selectionRowHeaderWidth: 35,
 	    	rowTemplate: rowTemplate,
 	    	data: data,
 		    columnDefs: [
@@ -28,19 +31,73 @@ function IngredientsCtrl(Ingredients, $scope, $q) {
 		      { name: 'description' }]
 		};
 
+    $scope.gridOptions.multiSelect = true;
+
+    $scope.setSelectable = function() {
+      $scope.gridApi.selection.clearSelectedRows();
+
+      $scope.gridOptions.isRowSelectable = function(row){
+        if(row.entity.age > 30){
+          return false;
+        } else {
+          return true;
+        }
+      };
+      $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.OPTIONS);
+
+      $scope.gridOptions.data[0].age = 31;
+      $scope.gridApi.core.notifyDataChange(uiGridConstants.dataChange.EDIT);
+    };
+
+    $scope.gridOptions.onRegisterApi = function(gridApi){
+      //set gridApi on scope
+      $scope.gridApi = gridApi;
+      gridApi.selection.on.rowSelectionChanged($scope,function(row){
+        var msg = 'row selected ' + row.isSelected;
+        $log.log(msg);
+      });
+
+      gridApi.selection.on.rowSelectionChangedBatch($scope,function(rows){
+        var msg = 'rows changed ' + rows.length;
+        $log.log(msg);
+      });
+    };
+
 		console.log(ingredients);
 
 		$q.when(ingredients, function(newData){
 			$scope.gridOptions.data = newData;
 		});
 
+    function addToCupboard(){
+      var selectedIngs = $scope.gridApi.selection.getSelectedGridRows();
+      console.log(selectedIngs);
+      selectedIngs = _.pluck(selectedIngs, 'entity');
+      console.log(selectedIngs);
+      Cupboard.bulkAdd(selectedIngs);
+    }
+
+    function addToShopping(){
+      var selectedIngs = $scope.gridApi.selection.getSelectedGridRows();
+      console.log(selectedIngs);
+      selectedIngs = _.pluck(selectedIngs, 'entity');
+      console.log(selectedIngs);
+      Shopping.bulkAdd(selectedIngs);
+    }
+
+    function clear(){
+      $scope.gridApi.selection.clearSelectedRows();
+    }
 
 
 	angular.extend(vm, {
-	  name: 'IngredientsCtrl'
+	  name: 'IngredientsCtrl',
+    addToShopping: addToShopping,
+    addToCupboard: addToCupboard,
+    clear: clear
 	});
 
-	
+
 }
 
 }());
