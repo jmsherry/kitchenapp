080a5308daa5d680bac4bebfd75d30e479189c8c
James Sherry <james.sherry@novafabrik.com>
2015-07-21 03:49:56 +0100

    removed extraneous routes; do not pull this commit, go next...

--- server/api/shopping/index.js
+++ server/api/shopping/index.js
@@ -4,10 +4,12 @@ var express = require('express');
 var router = express.Router();
 var controller = require('./shopping.controller');
 
+
+
 router.get('/', controller.getShoppingList);
 router.get('/:itemid', controller.getShoppingListItem);
 router.post('/', controller.addToShoppingList);
-router.put('/:itemid', controller.updateShoppingList);
+//router.put('/:itemid', controller.updateShoppingList);
 router.delete('/:itemid', controller.removeFromShoppingList);
 
 module.exports = router;
