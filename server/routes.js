d7ff61b7c383fff0efa65b6ee1633c4f35c5e824
James Sherry <james.sherry@novafabrik.com>
2015-07-21 03:46:27 +0100

    added middleware function to pick up stem variables in routes

--- server/routes.js
+++ server/routes.js
@@ -4,13 +4,19 @@ var config = require('./config/environment');
 
 module.exports = function (app) {
 
+  function paramFix(req, res, next) {
+      req._params = req.params;
+      next();
+  }
+
   // API
   app.use('/api/users', require('./api/user'));
   app.use('/api/ingredients', require('./api/ingredient'));
   app.use('/api/recipes', require('./api/recipe'));
-  app.use('/api/users/:userid/meals', require('./api/meal'));
-  app.use('/api/users/:userid/shopping', require('./api/shopping'));
-  app.use('/api/users/:userid/cupboard', require('./api/cupboard'));
+  app.use('/api/users/:userid/meals', paramFix, require('./api/meal'));
+  app.use('/api/users/:userid/shopping', paramFix, require('./api/shopping'));
+  app.use('/api/users/:userid/cupboard', paramFix, require('./api/cupboard'));
+
 
   // Auth
   app.use('/auth', require('./auth'));
