31b6bbd4d1581c5c69a6b6052613845b1a2fb539
James Sherry <james.sherry@novafabrik.com>
2015-08-07 05:35:12 +0100

    added a numbers only directive for form inputs

---
+++ client/directives/numbersOnly/numbersOnly.directive.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('kitchenapp')
+  .directive('numbersOnly', function () {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function(scope, element, attrs, modelCtrl) {
+
+       modelCtrl.$parsers.push(function (inputValue) {
+
+         var transformedInput = inputValue.replace(/[^0-9\.]+/g, '');
+
+         if (transformedInput!=inputValue) {
+           modelCtrl.$setViewValue(transformedInput);
+           modelCtrl.$render();
+         }
+
+         return transformedInput;
+       });
+      }
+    };
+  });
---
+++ client/directives/numbersOnly/numbersOnly.spec.js
@@ -0,0 +1,66 @@
+'use strict';
+
+describe('Directive: nav-bar', function () {
+
+  beforeEach(module('ngMock'));
+  beforeEach(module('ui.router'));
+  beforeEach(module('kitchenapp', 'templates'));
+
+
+  var $httpBackend,
+    $templateCache,
+    $compile,
+    $urlRouterProvider,
+    $rootScope,
+    $provide,
+    Auth,
+    element,
+    $scope,
+    links;
+
+    beforeEach(function(){
+
+      // module(function($provide){
+      //   $provide.decorator('Auth', function($delegate) {
+      //     $delegate.path = jasmine.createSpy();
+      //     return $delegate;
+      //   });
+      // });
+
+      // inject(function(_Auth_) {
+      //   Auth = _Auth_;
+      // });
+
+    });
+
+    beforeEach(inject(function (_$templateCache_) {
+      $templateCache = _$templateCache_;
+    }));
+
+
+  beforeEach(inject(function (_$httpBackend_, _$compile_, _$rootScope_) {
+
+
+
+    $httpBackend = _$httpBackend_;
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+
+
+    $scope = $rootScope.$new();
+    element = angular.element('<editIngredientForm></editIngredientForm>');
+    element = $compile(element)($scope);
+    $scope.$digest();
+
+    links = element.find('a');
+
+  }));
+
+  afterEach(function () {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+
+
+});
