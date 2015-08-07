e18e261541b1fae074c3ede5415af097a90b8b59
James Sherry <james.sherry@novafabrik.com>
2015-08-07 01:26:50 +0100

    added a test filter

---
+++ client/filters/currency.js
@@ -0,0 +1,7 @@
+// angular.module('kitchenapp').filter('currencyFilter', function () {
+// return function (value) {
+//     return value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+//   };
+// });
+
+//An example of a filter - found out how to use native currency filter with UI-Grid
