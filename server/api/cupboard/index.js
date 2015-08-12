2c67915e312da2c4b64331ccda48ef3def80fd0b
James Sherry <james.sherry@novafabrik.com>
2015-08-12 14:47:00 +0100

    corrected route to have an itemid; not necessary but helps with transparency

--- server/api/cupboard/index.js
+++ server/api/cupboard/index.js
@@ -7,7 +7,7 @@ var controller = require('./cupboard.controller');
 router.get('/', controller.getCupboard);
 router.get('/:itemid', controller.getCupboardItem);
 router.post('/', controller.addToCupboard);
-router.put('/', controller.updateCupboard);
+router.put('/:itemid', controller.updateCupboard);
 router.delete('/:itemid', controller.removeFromCupboard);
 
 module.exports = router;
