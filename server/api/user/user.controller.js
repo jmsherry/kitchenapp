e87be35e5ed47e5571230452e31a59d7afadbe4c
James Sherry <james.sherry@novafabrik.com>
2015-08-18 06:30:49 +0100

    stopped creation of cupboards, etc on registration

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -3,9 +3,9 @@
 var config = require('../../config/environment');
 var jwt = require('jsonwebtoken');
 var User = require('./user.model');
-var Cupboard = require('./../cupboard/cupboard.model');
-var Shopping = require('./../shopping/shopping.model');
-var Meals = require('./../meal/meal.model');
+// var Cupboard = require('./../cupboard/cupboard.model');
+// var Shopping = require('./../shopping/shopping.model');
+// var Meals = require('./../meal/meal.model');
 
 function handleError (res, err) {
   return res.status(500).send(err);
@@ -21,18 +21,18 @@ exports.create = function (req, res) {
   User.create(req.body, function (err, user) {
     if (err) { return handleError(res, err); }
 
-    req.body._id = null;
-    Cupboard.create({owner: user._id}, function (err, cupboard) {
-      if (err) { return handleError(res, err); }
-      console.log('Cuboard created for user: ', req.body);
-
-      Shopping.create({owner: user._id}, function (err, shopping) {
-        if (err) { return handleError(res, err); }
-        console.log('Shopping created for user: ', req.body);
-
-        Meals.create({owner: user._id}, function (err, meal) {
-          if (err) { return handleError(res, err); }
-          console.log('MealList created for user: ', req.body);
+    // req.body._id = null;
+    // Cupboard.create({owner: user._id}, function (err, cupboard) {
+    //   if (err) { return handleError(res, err); }
+    //   console.log('Cuboard created for user: ', req.body);
+    //
+    //   Shopping.create({owner: user._id}, function (err, shopping) {
+    //     if (err) { return handleError(res, err); }
+    //     console.log('Shopping created for user: ', req.body);
+    //
+    //     Meals.create({owner: user._id}, function (err, meal) {
+    //       if (err) { return handleError(res, err); }
+    //       console.log('MealList created for user: ', req.body);
 
           var token = jwt.sign(
             { _id: user._id },
@@ -41,11 +41,11 @@ exports.create = function (req, res) {
           );
           res.status(201).json({ token: token, user: user });
 
-        });
-
-      });
-
-    });
+    //     });
+    //
+    //   });
+    //
+    // });
 
   });
 };
