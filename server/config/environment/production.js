44a6416e01fc20b04f9bf35bf453e29b25ec7630
James Sherry <james.sherry@novafabrik.com>
2015-08-22 07:09:17 +0100

    heroku db setup config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -1,8 +1,16 @@
 'use strict';
 
-module.exports = {
-  ip: process.env.IP || undefined,
-  mongo: {
-    uri: 'mongodb://localhost/kitchenapp'
+module.exports = function(){
+  var mongoURI;
+  if(process.env.HEROKU){
+    mongoURI = 'mongodb://kitchenapp2:Lich1977@ds055732.mongolab.com:55732/heroku_t1mhd097';
+  } else {
+    mongoURI = 'mongodb://localhost/kitchenapp';
   }
+  return {
+    ip: process.env.IP || undefined,
+    mongo: {
+      uri: mongoURI
+    }
+  };
 };
--- server/config/environment/test.js
+++ server/config/environment/test.js
@@ -1,7 +1,15 @@
 'use strict';
 
-module.exports = {
-  mongo: {
-    uri: 'mongodb://localhost/kitchenapp-test'
+module.exports = function(){
+  var mongoURI;
+  if(process.env.HEROKU){
+    mongoURI = 'mongodb://kitchenapp2:Lich1977@ds055732.mongolab.com:55732/heroku_t1mhd097';
+  } else {
+    mongoURI = 'mongodb://localhost/kitchenapp-test';
   }
+  return {
+    mongo: {
+      uri: mongoURI
+    }
+  };
 };
