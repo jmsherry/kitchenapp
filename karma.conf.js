cfb825399fb64a4053a6427384a00d0a890bf075
James Sherry <james.m.sherry@googlemail.com>
2015-08-24 07:34:10 +0100

    added karma istanbul coverage

--- karma.conf.js
+++ karma.conf.js
@@ -73,15 +73,30 @@ module.exports = function (config) {
     ],
 
     //reporters: ['progress', 'coverage'],
-    //reporters: ['nyan', 'coverage'],
+    reporters: ['nyan', 'coverage'],
 
-    reporters: ['nyan'],
+    //reporters: ['nyan'],
     //reporters: ['nested'],
 
     coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    },
+          // specify a common output directory
+          dir: 'build/reports/coverage',
+          reporters: [
+            // reporters not supporting the `file` property
+            { type: 'html', subdir: 'report-html' },
+            { type: 'lcov', subdir: 'report-lcov' },
+            // reporters supporting the `file` property, use `subdir` to directly
+            // output them in the `dir` directory
+            { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+            { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+            { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+            { type: 'text', subdir: '.', file: 'text.txt' },
+            { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+          ],
+          instrumenterOptions: {
+            istanbul: { noCompact: true }
+          }
+        },
 
     port: 9876,
 
