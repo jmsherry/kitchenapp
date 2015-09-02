440d29c2b5a2dea8ea735afcbfaf94748bb3478d
James Sherry <james.m.sherry@googlemail.com>
2015-09-02 22:02:05 +0100

    jslint corrections

--- client/filters/currency.js
+++ client/filters/currency.js
@@ -1,8 +1,12 @@
 (function(){
-  angular.module('kitchenapp').filter('currencyFilter', function () {
-  return function (value) {
+  'use strict';
+
+  angular.module('kitchenapp')
+  .filter('currencyFilter', function () {
+    return function (value) {
       return value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
     };
   });
+
 }());
 //An example of a filter
