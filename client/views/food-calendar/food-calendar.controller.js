ce888e70f6439191d1c1f31c4383185395aa2e9e
James Sherry <james.m.sherry@googlemail.com>
2015-09-27 15:49:45 +0100

    work on modals and responsive for cal controls

--- client/views/food-calendar/food-calendar.controller.js
+++ client/views/food-calendar/food-calendar.controller.js
@@ -12,30 +12,32 @@
 
     var meals, completeMeals, placedMeals, vm = this;
 
+    function wrap(element, index, array){
+
+      var eventWrapper = Object.create(element);
+      eventWrapper = angular.extend(eventWrapper, {
+        title: element.name, // The title of the event
+        type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special
+        startsAt: moment(element.startsAt).toDate(), // A javascript date object for when the event starts
+        endsAt: moment(element.startsAt).add(1, 'h').toDate(), // Optional - a javascript date object for when the event ends
+        editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable.
+        deletable: true, // If delete-event-html is set and this field is explicitly set to false then dont make it deleteable
+        draggable: true, //Allow an event to be dragged and dropped
+        resizable: true, //Allow an event to be resizable
+        incrementsBadgeTotal: true, //If set to false then will not count towards the badge total amount on the month and year view
+        //recursOn: 'week', // If set the event will recur on the given period. Valid values are year or month
+        cssClass: 'balls' //A CSS class (or more, just separate with spaces) that will be added to the event when it is displayed on each view. Useful for marking an event as selected / active etc
+      });
+
+      return eventWrapper;
+    }
+
     meals = Meals.get();
     $q.when(meals, function (data) {
       var mls = data.complete;
       completeMeals = _.filter(mls, 'startsAt', null);
       placedMeals = _.reject(mls, 'startsAt', null);
-      vm.events = placedMeals.map(function(element, index, array){
-
-        var eventWrapper = Object.create(element);
-        eventWrapper = angular.extend(eventWrapper, {
-          title: element.name, // The title of the event
-          type: 'info', // The type of the event (determines its color). Can be important, warning, info, inverse, success or special
-          startsAt: moment(element.startsAt).toDate(), // A javascript date object for when the event starts
-          endsAt: moment(element.startsAt).add(1, 'h').toDate(), // Optional - a javascript date object for when the event ends
-          editable: false, // If edit-event-html is set and this field is explicitly set to false then dont make it editable.
-          deletable: true, // If delete-event-html is set and this field is explicitly set to false then dont make it deleteable
-          draggable: true, //Allow an event to be dragged and dropped
-          resizable: true, //Allow an event to be resizable
-          incrementsBadgeTotal: true, //If set to false then will not count towards the badge total amount on the month and year view
-          //recursOn: 'week', // If set the event will recur on the given period. Valid values are year or month
-          cssClass: 'balls' //A CSS class (or more, just separate with spaces) that will be added to the event when it is displayed on each view. Useful for marking an event as selected / active etc
-        });
-
-        return eventWrapper;
-      });
+      vm.events = placedMeals.map(wrap);
 
       $log.log("meals data: ", data);
       $log.log("mls", mls);
@@ -98,6 +100,7 @@
       // }];
 
       vm.completeMeals = completeMeals;
+      vm.placedMeals = placedMeals;
 
       function showModal(day) {
         //var self = this;
@@ -107,16 +110,22 @@
           controller: function modalController($modalInstance, $scope) {
             //var vm = this;
             $log.log('modal controller running', arguments, placedMeals);
-            $scope.selectedDate = day;
+
             $scope.completeMeals = completeMeals;
+            $scope.placedMeals = placedMeals;
+            $scope.selectedDate = day;
             $scope.placeMeal = function (meal, date) {
               var $mealPlaced;
               $log.log('Placing', arguments);
               meal.startsAt = day.date.toString();
               $mealPlaced = Meals.update(meal);
               $q.when($mealPlaced, function(pMl){
-                placedMeals.push(vm.wrap(meal));
+                var scheduledMeal = vm.wrap(meal);
+                vm.events.splice(Utils.collIndexOf(vm.completeMeals, meal), 1);
+                placedMeals.push(scheduledMeal);
               });
+
+              event.stopPropagation();
             };
             //$log.log('modal scope', vm);
           },
@@ -201,7 +210,8 @@
         drillDownClick: drillDownClick,
         weekdayClick: weekdayClick,
         addClicked: addClicked,
-        eventTimesChanged: eventTimesChanged
+        eventTimesChanged: eventTimesChanged,
+        wrap: wrap
       });
 
     });
