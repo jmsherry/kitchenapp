5bedf9091a4c034a6092f96bcd63562807852bd7
James Sherry <james.m.sherry@googlemail.com>
2015-10-16 08:33:51 +0100

    corrected required file name

--- tasks/apidoc.js
+++ tasks/apidoc.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp'),
   apidoc = require('gulp-apidoc'),
-  apidoc2md = require('gulp-apidoc-markdown');
+  apidoc2md = require('gulp-apidoc-to-markdown');
 
 module.exports = function (done) {
-  
+
   apidoc2md.generate(process.cwd() + "/dist/docs/", process.cwd() + "/dist/docs/api.md");
 
   apidoc({
