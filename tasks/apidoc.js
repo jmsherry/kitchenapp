bd7096b210ffa6e0f09567989e94b4ce3f18c09b
James Sherry <james.m.sherry@googlemail.com>
2015-10-16 19:28:24 +0100

    corrected entry

--- tasks/apidoc.js
+++ tasks/apidoc.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
-  apidoc = require('gulp-apidoc');//,
-  //apidoc2md = require('gulp-apidoc-to-markdown');
+  apidoc = require('gulp-apidoc'),
+  apidoc2md = require('gulp-apidoc-to-markdown');
 
 module.exports = function (done) {
 
-  //apidoc2md.generate(process.cwd() + "/docs/", process.cwd() + "/docs/api.md");
+  apidoc2md.generate(process.cwd() + "/docs/api", process.cwd() + "/docs/api/api.md");
 
   apidoc({
     src: "./server/",
