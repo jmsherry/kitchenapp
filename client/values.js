f171f84c1d49fd72d60d430838896265d72ff9fe
James Sherry <james.m.sherry@googlemail.com>
2015-10-21 21:26:07 +0100

    values file

---
+++ client/values.js
@@ -0,0 +1,13 @@
+/* global angular:true */
+(function() {
+    'use strict';
+
+
+    // 'Global' variables used by the entire app //Not actually used!!
+    angular
+        .module('kitchenapp')
+        .value('currentLang', 'en-gb');
+        .value('currentLocale', 'en-gb');
+
+
+})();
