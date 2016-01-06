27240a1153dfbd4f4a0daa5fa7f417d9b8f3a42b
James Sherry <james.m.sherry@googlemail.com>
2016-01-06 16:56:36 +0000

    corrected chocolate routes for class

--- server/routes.js
+++ server/routes.js
@@ -70,12 +70,12 @@
         thumbnailURL: 'http://img.tesco.com/Groceries/pi/476/5000159470476/IDShot_110x110.jpg'
       }, {
         name: 'twix',
-        cost: 0.7
+        cost: 0.7,
         imageURL: 'http://img.tesco.com/Groceries/pi/228/5000159459228/IDShot_540x540.jpg',
         thumbnailURL: 'http://img.tesco.com/Groceries/pi/228/5000159459228/IDShot_110x110.jpg'
       }, {
         name: 'Galaxy Caramel (135g)',
-        cost: 1.5
+        cost: 1.5,
         imageURL: 'http://img.tesco.com/Groceries/pi/710/5900951027710/IDShot_540x540.jpg',
         thumbnailURL: 'http://img.tesco.com/Groceries/pi/710/5900951027710/IDShot_110x110.jpg'
       }]);
@@ -89,7 +89,7 @@
         thumbnailURL: 'http://img.tesco.com/Groceries/pi/476/5000159470476/IDShot_110x110.jpg'
       }, {
         name: 'twix',
-        cost: 0.7
+        cost: 0.7,
         imageURL: 'http://img.tesco.com/Groceries/pi/228/5000159459228/IDShot_540x540.jpg',
         thumbnailURL: 'http://img.tesco.com/Groceries/pi/228/5000159459228/IDShot_110x110.jpg'
       }, {
