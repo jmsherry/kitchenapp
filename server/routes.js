45d1967b9719f856f947db6d58c660ee70e8e251
James Sherry <james.m.sherry@googlemail.com>
2016-01-07 00:02:20 +0000

    corrected missing comma

--- server/routes.js
+++ server/routes.js
@@ -94,7 +94,7 @@
         thumbnailURL: 'http://img.tesco.com/Groceries/pi/228/5000159459228/IDShot_110x110.jpg'
       }, {
         name: 'snickers',
-        cost: 0.7
+        cost: 0.7,
         imageURL: 'http://img.tesco.com/Groceries/pi/476/5000159470476/IDShot_540x540.jpg',
         thumbnailURL: 'http://img.tesco.com/Groceries/pi/476/5000159470476/IDShot_110x110.jpg'
       }]);
